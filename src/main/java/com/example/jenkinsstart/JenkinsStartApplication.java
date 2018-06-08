b21cfd3f180687f502e5f0eb1086f8449c24a65d
Musodza, Trevor Shamiso : Barclays Africa Group Limited [X] <Trevor.Musodza@absa.co.za>
2018-06-08 12:33:31 +0200

    app shell

---
+++ src/main/java/com/example/jenkinsstart/JenkinsStartApplication.java
@@ -0,0 +1,12 @@
+package com.example.jenkinsstart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsStartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsStartApplication.class, args);
+	}
+}
